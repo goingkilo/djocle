48fae960c0be2e8c868ddecb8bec65f9cd5b51b4
karthik raghunathan <kraghunathan@nile-global.com>
2022-06-07 21:09:06 +0530

    .

--- mytodo1/settings.py
+++ mytodo1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mytodo1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +70,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mytodo1.wsgi.application'
 
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
--- mytodo1/urls.py
+++ mytodo1/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 from . import views
+from django.contrib import admin
 
 """mytodo1 URL Configuration
 
@@ -18,10 +20,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mypics/', include('mypics.urls'))
+    path('mypics/', include('mypics.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
 ]
